b16d7ce63ff17e88bd5f652c5b1a50ec9c99ab8d
Sherif Lashin <sherifcrew94@gmail.com>
2022-11-01 00:43:09 +0200

    made a change

---
+++ JWT/Controllers/CustomersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    //[EnableCors("EnableCORS")]
+    public class CustomersController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public async Task<IEnumerable<string>> Get() => new string[] { "Jon Doe", "John Doe" };
+    }
+}
---
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './Components/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
